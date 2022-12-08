714263b753f8ded3a9e47463b0c990ee514b0269
jul003 <dzulkiflifaiz11@gmail.com>
2022-12-08 16:07:23 +0700

    modifaneuy

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Mangekyou_Sharingan_Sasuke.svg/2048px-Mangekyou_Sharingan_Sasuke.svg.png" className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          EMangekyō Sharingan (万華鏡写輪眼) adalah sebuah bentuk peningkatan dari Sharingan yang hanya dapat diaktifkan oleh sebagian anggota Klan Uchiha.[1] Mata ini tercatat menjadi "mata surgawi yang melihat kebenaran dari seluruh ciptaan tanpa halangan" (天壌の理を掌せし瞳, tenjō no kotowari o shōaku seshi hitomi).[2]
+        </p>
+        <p>
+        Mangekyō Sharingan memiliki perbedaan dari Sharingan normal dari segi penampilannya, yang mengubah bentuk tomoe. Desain pasti berbeda dari satu pengguna dengan pengguna yang lain, meskipun pada dasarnya semua menyerupai bentuk kincir angin. Mata ini awalnya bangkit oleh trauma pengguna stelah menyaksikan kematian seseorang yang dekat dengan pengguna. Uchiha secara historis salah memahami kondisi ini sebagai pengguna perlu bertanggung jawab atas kematian seseorang itu, dan untuk alasan itu mereka mengembangkan praktek membunuh teman terdekat mereka untuk mendapatkan Mangekyō Sharingan.[3] Mangekyō milik Obito diaktifkan segera setelah traumanya, sedangkan di Naruto Jinraiden, Mangekyō milik Sasuke baru dapat diaktifkan beberapa minggu setelah kematian Itachi.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://naruto.fandom.com/id/wiki/Mangeky%C5%8D_Sharingan"
           target="_blank"
           rel="noopener noreferrer"
         >
